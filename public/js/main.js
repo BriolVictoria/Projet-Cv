7a53a0e3866344ffaa345c00c3ea69ae3680a2d3
BriolVictoria <victoria.briol@student.hepl.be>
2025-06-02 10:45:06 +0200

    Added js, added new picture and added some html and css for the langage content

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,17 @@ const buttons = document.querySelectorAll('.parcours_section__projets_liens__lie
 const sections = document.querySelectorAll('.parcours_section__contenu > div');
 
 buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        console.log('ça marche');
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const target = button.dataset.target; // Je dois l'initialiser ici car au dessus ça ne marche pas, demander prof
+
+        sections.forEach(section => {
+            section.classList.add('hidden');
+
+            if (section.classList.contains(`parcours_section__contenu__${target}`)) { // Demander si on peut faire ça ou pas
+                section.classList.remove('hidden');
+            }
+        });
     });
 });
