ba037d3172d0c2c5abaca3cb52b2cd52f05a8b4c
BriolVictoria <victoria.briol@student.hepl.be>
2025-06-03 14:54:14 +0200

    Added new video, added the html and css and Javascript

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const buttons = document.querySelectorAll('.parcours_section__projets_liens__lien');
 const sections = document.querySelectorAll('.parcours_section__contenu > div');
+const boutons = document.querySelectorAll('.parcours_section__expériences__liens__lien');
+const blocs = document.querySelectorAll('.parcours_section__expériences__contenu > div');
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
@@ -12,6 +14,23 @@ buttons.forEach(button => {
 
             if (section.classList.contains(`parcours_section__contenu__${target}`)) { // Demander si on peut faire ça ou pas
                 section.classList.remove('hidden');
+            }
+        });
+    });
+});
+
+
+
+boutons.forEach(bouton => {
+    bouton.addEventListener('click', e => {
+        e.preventDefault();
+
+        const cible = bouton.dataset.target.toLowerCase();
+
+        blocs.forEach(bloc => {
+            bloc.classList.add('hidden');
+            if (bloc.className.includes(`contenu__${cible}`)) {
+                bloc.classList.remove('hidden');
             }
         });
     });
