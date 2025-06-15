d20cf63c829ee5b1d505740895d4d97d6c7f3a71
BriolVictoria <victoria.briol@student.hepl.be>
2025-06-15 13:13:28 +0200

    Added readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# CV 
+
+## Description
+
+Ce projet est un CV en ligne conçu à partir d’un site existant, que j’ai personnalisé afin d’y intégrer mes compétences, mes réalisations et mon parcours professionnel.
+Le site est développé en **HTML**, **CSS** et **JavaScript**
+---
+
+## Fonctionnalités
+
+- Montrer mes capacités, ainsi que mon travail
+- Montrer mon expériences
+
+---
+
+## Technologies utilisées
+
+- HTML5
+- CSS3
+- JavaScript
+
+---
+
+## Maquettes utilisés pendant la réalisation du CV
+
+- Figma: https://www.figma.com/design/OFLh6j0VzeCCHlEyQTG6la/CV?node-id=1-13&t=oaA5TPmLxK0ABkOc-1
+
+---
+
+## Retrouvés mon CV
+-  [Victoria-briol.be](https://victoria-briol.be/)
+
+## Retrouvés le site de base
+-  https://www.jetbrains.com/phpstorm/
+
---
+++ dist/js/main.js
@@ -0,0 +1,47 @@
+const buttons = document.querySelectorAll('.parcours_section__projets_liens__lien');
+const sections = document.querySelectorAll('.parcours_section__contenu > div');
+const boutons = document.querySelectorAll('.parcours_section__expériences__liens__lien');
+const blocs = document.querySelectorAll('.parcours_section__expériences__contenu > div');
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const target = button.dataset.target; // Je dois l'initialiser ici car au dessus ça ne marche pas, demander prof
+
+        sections.forEach(section => {
+            section.classList.add('hidden');
+
+            if (section.classList.contains(`parcours_section__contenu__${target}`)) { // Demander si on peut faire ça ou pas
+                section.classList.remove('hidden');
+            }
+        });
+    });
+});
+
+
+
+boutons.forEach(bouton => {
+    bouton.addEventListener('click', e => {
+        e.preventDefault();
+
+        const cible = bouton.dataset.target.toLowerCase();
+
+        blocs.forEach(bloc => {
+            bloc.classList.add('hidden');
+            if (bloc.className.includes(`contenu__${cible}`)) {
+                bloc.classList.remove('hidden');
+            }
+        });
+    });
+});
+
+const burgerBtn = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+burgerBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+
+    const expanded = burgerBtn.getAttribute('aria-expanded') === 'true';
+    burgerBtn.setAttribute('aria-expanded', !expanded);
+});
